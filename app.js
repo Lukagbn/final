def74ee64e7aa6f3bafec2be76c6f8e1e64ac6d1
lukagbn <luka68690@gmail.com>
2025-12-17 00:19:46 +0400

    add dialog, novalidate to form, and eventlistener to form inputs

--- app.js
+++ app.js
@@ -25,10 +25,10 @@ const options = {
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      sliderAnimation(0, 72, 70);
-      sliderAnimation(1, 50, 48);
-      sliderAnimation(2, 60, 58);
-      sliderAnimation(3, 84, 82);
+      sliderAnimation(0, 95, 93);
+      sliderAnimation(1, 96, 94);
+      sliderAnimation(2, 96, 94);
+      sliderAnimation(3, 98, 96);
       observer.unobserve(entry.target);
     }
     return;
@@ -144,4 +144,76 @@ btnContainer.forEach((el) => {
     btnContainer.forEach((btn) => btn.removeAttribute("active"));
     el.setAttribute("active", "");
   });
+});
+
+const contactForm = document.querySelector("form");
+const dialog = document.getElementById("dialog");
+const body = document.querySelector("body");
+const nameInput = contactForm.querySelector(".name");
+const emailInput = contactForm.querySelector(".email");
+const webInput = contactForm.querySelector(".yourWeb");
+const messageInput = contactForm.querySelector(".yourMessage");
+const gmailPattern = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+
+contactForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const emailPattern = /^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$/;
+  fetch("https://borjomi.loremipsum.ge/api/send-message", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+    body: JSON.stringify({
+      name: nameInput.value.trim(),
+      email: emailInput.value.trim(),
+      web: webInput.value.trim(),
+      message: messageInput.value.trim(),
+    }),
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json))
+    .catch((err) => console.error(err));
+  dialog.showModal();
+  body.classList.add("overFlow-hidden");
+  if (nameInput.value.trim().length < 3) {
+    console.log("too short name");
+  }
+  if (!emailPattern.test(emailInput.value.trim())) {
+    console.log("try again");
+  }
+});
+const closeContact = document.querySelector(".cross");
+closeContact.addEventListener("click", (e) => {
+  dialog.close();
+  body.classList.remove("overFlow-hidden");
+  contactForm.reset();
+});
+document.addEventListener("click", (e) => {
+  if (e.target === dialog) {
+    dialog.close();
+    body.classList.remove("overFlow-hidden");
+    contactForm.reset();
+  }
+});
+nameInput.addEventListener("input", (e) => {
+  if (e.target.value.length < 8) {
+    console.log("try again");
+  }
+});
+emailInput.addEventListener("input", (e) => {
+  if (gmailPattern.test(e.target.value)) {
+    console.log("good");
+  } else {
+    console.log("invalid");
+  }
+});
+webInput.addEventListener("input", (e) => {
+  console.log(e.target.value);
+});
+messageInput.addEventListener("input", (e) => {
+  if (e.target.value.length < 50) {
+    console.log("message words are too short!");
+  } else {
+    console.log("looks good!");
+  }
 });
